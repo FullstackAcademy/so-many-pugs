aac49a698a63151086fee5ca46181396686698e4
Thomas Kelly <tmkelly28@gmail.com>
2018-04-23 11:58:41 -0400

    add security middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use(require('./logging'))
+router.use(require('./security'))
 router.use(require('./body-parsing'))
 router.use(require('./compression'))
 router.use(require('./session'))
