f3b0740dd1f7a5d83f5984440863f06315a90751
Thomas Kelly <tmkelly28@gmail.com>
2018-04-23 11:14:53 -0400

    add compression middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -3,5 +3,6 @@ module.exports = router
 
 router.use(require('./logging'))
 router.use(require('./body-parsing'))
+router.use(require('./compression'))
 router.use(require('./session'))
 router.use(require('./static'))
